51a88e66da12ed0b05a2635b0157abbfec3055f6
Halyna KYRYLIUK <h.kyryliuk@evolucare.com>
2024-10-15 15:12:25 +0200

    restore and add protocols

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { ModuleNavigationComponent } from './module-navigation/module-navigation.component';
 import { MatTreeModule } from '@angular/material/tree';
 import { MatIconModule } from '@angular/material/icon';
+import { ProtocolsComponent } from './protocols/protocols.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ModuleNavigationComponent,
+    AppComponent, ModuleNavigationComponent, ProtocolsComponent,
   ],
   imports: [
     BrowserModule, MatTreeModule, MatIconModule
