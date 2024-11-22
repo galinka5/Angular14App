06c2312d00543a2364270ee98faa9b8136975c0d
h.kyryliuk <h.kyryliuk@evolucare.com>
2024-11-22 11:23:42 +0100

    validation doctor dialog

--- src/app/dialogs/validationDoctor-dialog/validationDoctor-dialog.component.ts
+++ src/app/dialogs/validationDoctor-dialog/validationDoctor-dialog.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { TranslateText } from '../../QCare/Helper';
+//import { HttpService } from '../../../services/http.service';
 
 @Component({
-  selector: 'app-validation-doctor-dialog',
+  selector: 'validationDoctor-dialog',
   templateUrl: './validationDoctor-dialog.component.html',
-  styleUrl: './validationDoctor-dialog.component.css'
+  styleUrls: ['./validationDoctor-dialog.component.css']
 })
-export class ValidationDoctorDialog {
+export class ValidationDoctorDialog implements OnInit {
+  public title: string = TranslateText('Select doctor');
 
+  public doctorList: { key: number, name: string }[] = [{ key: 1, name: "string" }, { key: 2, name: "asdasdasd asd  kjasbdksjf kjsd fsdf" }];
+  private fullDoctorList: { key: number, name: string }[] = [];
+
+  public searchPlaceHolderText: string = "";
+
+  public selectedDoctorKey: number = 0;
+  public selectedDoctorName: string = "";
+
+  public textSave: string = TranslateText('Save');
+  public textCancel: string = TranslateText('Annuler');
+  constructor(
+    public dialogRef: MatDialogRef<ValidationDoctorDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: { allowWardDoctor: boolean, selectedDoctorKey: number},
+  ) { }
+
+  ngOnInit(): void {
+
+    this.selectedDoctorKey = this.data.selectedDoctorKey;
+    this.searchPlaceHolderText = TranslateText('Search');
+
+    if (this.data.allowWardDoctor) {
+      this.doctorList.push({ key: -1, name: TranslateText('Ward doctor responsible for the shift') })
+    }
+
+    /*this.httpService.httpCall('GET', 'api/User/GetAllDoctorsForService').subscribe(
+      (response) => {
+        response.forEach(doctor => {
+          this.doctorList.push({ key: doctor.Key, name: doctor.FirstName + ' ' + doctor.LastName });
+          this.fullDoctorList.push({ key: doctor.Key, name: doctor.FirstName + ' ' + doctor.LastName });
+        })
+      },
+      (error) => {
+        console.error('API request for GetAllDoctorsForService failed:', error);
+      }
+    );*/
+
+
+
+
+
+  }
+
+  filterList(textToFilter: string) {
+    this.doctorList = this.fullDoctorList.filter(x => x.name.toUpperCase().includes(textToFilter.toUpperCase()));
+  }
+
+  selectElement(element: any) {
+    this.selectedDoctorKey = element.key;
+    this.selectedDoctorName = element.name;
+    this.dialogRef.close({
+      doctorKey: this.selectedDoctorKey,
+      doctorName: this.selectedDoctorName
+    });
+  }
+
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
